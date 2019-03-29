c46609a890ed92820e8c58f369086dd9bc739d6f
jattoabdul <jattoade@gmail.com>
2019-03-29 01:03:35 +0100

    add courses and join us sections

--- pages/index.vue
+++ pages/index.vue
@@ -19,106 +19,68 @@
           </div>
         </div>
       </nav>
-      <main class="banner" @contextmenu.stop>
+      <div class="banner" @contextmenu.stop>
         <div class="content">
           <h1 class="heading">
-            Community Driven
+            Frontend Learning Resource Path
           </h1>
-          <p>Discover the value of an open, collaborative effort by one of the largest open-source teams</p>
+          <p>
+            Discover the value of an open, collaborative effort by one of the largest open-source teams
+            <br>
+            <small>- by Devcenter Square</small>
+          </p>
           <button>Start Learning</button>
         </div>
         <img class="dc-center" src="/svg/dc-img.svg" alt="dc-center">
-      </main>
+      </div>
     </header>
+    <main class="main-content">
+      <section class="courses">
+        <h2>Everyone Benefits</h2>
+        <p class="sub-text">
+          By being open and freely available, it enables and encourages collaboration and <br> the development of technology, solving real world problems.
+        </p>
+        <div class="course-lists">
+          <div v-for="(course, courseIndex) in courses" :key="courseIndex" class="course card center-card">
+            <div class="card-content">
+              <h2 class="course-title" v-text="course.title" />
+              <p class="course-level" :style="{ color: course.color }" v-text="course.level" />
+              <a class="btn" :href="course.link" target="_blank">
+                <span :style="{ background: course.color }">Open Course</span>
+                <img src="/svg/study.svg" alt="study">
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="join-us">
+        <h2>Join the Community</h2>
+        <p>Learn what it means to join us &amp; contribute, find and <br> get started on a new project.</p>
+        <a class="join-us-btn" href="#">Join Us</a>
+        <img src="/svg/dc-people.svg" alt="join the community of tech people">
+      </section>
+      <section class="contributors">
+        
+      </section>
+      <section class="supporters-and-socials" />
+    </main>
+    <footer class="footer" />
   </div>
 </template>
 
 <script>
-
-export default {}
-</script>
-
-<style lang="scss">
-.page {
-  header {
-    background: url('/svg/dc-bg.svg');
-    background-size: cover;
-    // height: 100vh;
-  }
-  nav {
-    display: flex;
-    justify-content: space-between;
-    padding: 5vh 2.5vw 0;
-
-    .logo {
-      img {
-        height: 3rem;
-      }
-    }
-
-    .nav-links {
-      display: flex;
-      align-items: center;
-      a {
-        padding: 0 1rem;
-        &.active {
-          font-weight: 600;
-        }
-      }
-
-      .double-btn-links {
-        margin-left: 1rem;
-        display: flex;
-        a {
-          padding: 0.5rem 1.25rem;
-          color: white;
-          background: $deep-blue;
-          display: flex;
-          align-items: center;
-
-          &:first-child {
-            border-top-left-radius: 4px;
-            border-bottom-left-radius: 4px;
-            border-right: 1px solid $dark-blue;
-          }
-
-          &:last-child {
-            border-top-right-radius: 4px;
-            border-bottom-right-radius: 4px;
-          }
-
-          img {
-            height: 0.75rem;
-            margin-left: 0.5rem;
-          }
-        }
-      }
-    }
-  }
-
-  .banner {
-    display: grid;
-    grid-template-columns: 2fr 3fr;
-    padding: 10vh 2.5vw;
-
-    .content {
-      padding-right: 5vw;
-    }
-
-    .heading {
-      font-size: 2.5rem;
-      font-weight: 500;
-    }
-
-    .dc-center {
-      max-width: 100%;
-    }
-
-    button {
-      background: $green;
-      border-radius: 10px;
-      margin-top: 2rem;
+export default {
+  data() {
+    return {
+      courses: [
+        { title: 'front end development', level: 'beginner', color: '#2CA0FB', link: 'https://learning-resource-path.gitbook.io/resources/#beginner-level-front-end-development' },
+        { title: 'frontend development', level: 'mid-level', color: '#3DC7C9', link: 'https://learning-resource-path.gitbook.io/resources/#mid-level-front-end-development' },
+        { title: 'frontend development', level: 'advanced', color: '#EB4A34', link: 'https://learning-resource-path.gitbook.io/resources/#advanced-level-front-end-development' },
+        { title: 'frontend development', level: 'expert', color: '#FBA12C', link: 'https://learning-resource-path.gitbook.io/resources' }
+      ]
     }
   }
 }
-</style>
+</script>
+
+<style lang="scss" src="./index.scss" scoped />
