7e662f268c7f2a0ae5529d0968d16c27f8765d92
jattoabdul <jattoade@gmail.com>
2019-03-29 10:02:57 +0100

    fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,8 @@ export default {
     scss: './assets/scss/variables.scss'
   },
   router: {
-    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/dev-center-lrp/' : '/',
+    // base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/dev-center-lrp/' : '/',
+    base: process.env.NODE_ENV === 'dev' ? '/' : './',
     linkExactActiveClass: 'active'
   },
 
